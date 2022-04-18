814ca10a6aceb1573c22500410f4030e75843085
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-04-18 11:58:27 +0300

    Navigation compose

--- app/src/main/java/com/asimodabas/crypto_tracking_v2/MainActivity.kt
+++ app/src/main/java/com/asimodabas/crypto_tracking_v2/MainActivity.kt
@@ -8,8 +8,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import com.asimodabas.crypto_tracking_v2.ui.theme.CryptoTrackingv2Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +23,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoTrackingv2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+
+                val navController = rememberNavController()
+
+                
+                NavHost(navController = navController, startDestination = "crypto_list_screen" ){
+
+                    composable("crypto_list_screen"){
+                        //Screen
+
+                    }
+
+                    composable("crypto_detail_screen/{cryptoId}/{cryptoPrice}", arguments = listOf(
+                        navArgument("cryptoId"){
+                            type = NavType.StringType
+                        },
+                        navArgument("cryptoPrice"){
+                            type= NavType.StringType
+                        }
+                    )){
+                        //Detail Screen
+
+                        val cryptoId = remember {
+                            it.arguments?.getString("cryptoId")
+                        }
+                        val cryptoPrice = remember {
+                            it.arguments?.getString("cryptoPrice")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CryptoTrackingv2Theme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/asimodabas/crypto_tracking_v2/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.asimodabas.crypto_tracking_v2.util
+
+object Constants {
+
+    // https://raw.githubusercontent.com/asimodabas/Crypto-Tracking-v2/master/cryptolist.json
+    // https://raw.githubusercontent.com/asimodabas/Crypto-Tracking-v2/master/crypto.json
+
+    const val BASE_URL = "https://raw.githubusercontent.com"
+    const val API_KEY = "_____________"
+    const val CALL_ATTRIBUTES = "id,name,logo_url"
+
+}
