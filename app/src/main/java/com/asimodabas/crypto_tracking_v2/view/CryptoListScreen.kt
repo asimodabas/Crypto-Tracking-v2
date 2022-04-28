f9aae2dba68a7840973df89ac48cc553e1915a54
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-04-28 16:35:08 +0300

    list screen+ and manifest

--- app/src/main/java/com/asimodabas/crypto_tracking_v2/view/CryptoListScreen.kt
+++ app/src/main/java/com/asimodabas/crypto_tracking_v2/view/CryptoListScreen.kt
@@ -1,10 +1,23 @@
 package com.asimodabas.crypto_tracking_v2.view
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.asimodabas.crypto_tracking_v2.viewmodel.CryptoListViewModel
 
 @Composable
-fun CryptoListScreen(navController: NavController) {
-    Text("Hello")
+fun CryptoListScreen(navController: NavController,
+viewModel:CryptoListViewModel = hiltViewModel()
+) {
+
+    Surface(
+        color = MaterialTheme.colors.error, modifier = Modifier.fillMaxSize()
+    ){
+
+    }
+
 }
